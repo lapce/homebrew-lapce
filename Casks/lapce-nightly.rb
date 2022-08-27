5bde450725af5d80d6b76377c5360ec15ff8c512
Jakub Panek <me@panekj.dev>
2022-08-27 20:31:52 +0200

    casks: add lapce{,-nightly}.rb

---
+++ Casks/lapce-nightly.rb
@@ -0,0 +1,19 @@
+cask "lapce-nightly" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/lapce/lapce/releases/download/nightly/Lapce-macos.dmg",
+    verified: "github.com/lapce/lapce/"
+  name "Lapce (Nightly)"
+  desc "Lightning-fast and Powerful Code Editor written in Rust"
+  homepage "https://lapce.dev/"
+
+  app "Lapce.app"
+
+  uninstall quit: "io.lapce"
+
+  zap trash: [
+    "~/Library/Application Support/dev.lapce.Lapce-nightly",
+    "~/Library/Saved Application State/io.lapce.savedState",
+  ]
+end
---
+++ Casks/lapce.rb
@@ -0,0 +1,20 @@
+cask "lapce" do
+  version "0.1.3"
+  sha256 "edb15bcb9c8b79d0b7c52621ffdebb41a4e6df6016ebefeb2f6e1e89beec76af"
+
+  url "https://github.com/lapce/lapce/releases/download/v#{version}/Lapce-macos.dmg",
+    verified: "github.com/lapce/lapce/"
+  name "Lapce"
+  desc "Lightning-fast and Powerful Code Editor written in Rust"
+  homepage "https://lapce.dev/"
+
+  app "Lapce.app"
+
+  uninstall quit: "io.lapce"
+
+  zap trash: [
+    "~/.lapce",
+    "~/Library/Application Support/Lapce",
+    "~/Library/Saved Application State/io.lapce.savedState",
+  ]
+end
